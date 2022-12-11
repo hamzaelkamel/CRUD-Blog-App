0bede7f9d7fdc7cdc291250d776d61bc389e7508
hamzaelkamel <elkamelh25@gmail.com>
2022-12-11 15:37:43 +0100

    create Entite Article and CRUD of it

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use App\Form\ArticleType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+     #[Route('/article', name: 'app_article')]
+    public function index(ArticleRepository $repository): Response
+    {         
+        return $this->render('article/index.html.twig', [
+            'articles' =>$repository->findAll(),
+        ]);
+    }
+
+    #[Route('/article/new','article.new',methods:['GET','POST'])]
+    public function new( Request $request, EntityManagerInterface $manager): Response
+    {
+        $article = new article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $article = $form->getData();
+            
+            $manager->persist($article);
+            $manager->flush();
+           
+            $this->addFlash(
+                'success',
+                'The Article is Add With Success !'
+            );
+            return $this->redirectToRoute('app_article');
+        }
+         return $this->render('article/new.html.twig', [
+            'form'=> $form->createView()
+         ]);
+    }
+
+    
+   #[Route('article/edit/{id}','article_edit', methods: ['GET','POST'])]
+   public function edit( Article $article , Request $request, EntityManagerInterface $manager ): Response
+   {
+   
+    $form = $this->createForm(ArticleType::class, $article);
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid()){
+        $article = $form->getData();
+        
+        $manager->persist($article);
+        $manager->flush();
+       
+        $this->addFlash(
+            'success',
+            'The Article is Modify With Success !'
+        );
+        return $this->redirectToRoute('app_article');
+
+    }
+     return $this->render('article/edit.html.twig' , [
+        'form' => $form->createView()
+     ]);
+    }
+
+    #[Route('article/delete/{id}','article_delete', methods: ['GET'])]
+    public function delete(EntityManagerInterface $manager, Article $article ): Response
+    {
+       if(!$article){
+        $this->addFlash(
+            'success',
+            'The Article is Deleted With Success !'
+        );
+        return $this->redirectToRoute('app_article');
+       }
+       $manager->remove($article);
+       $manager->flush();
+       $this->addFlash(
+        'success',
+        'The Article is Deleted With Success !'
+       );
+        return $this->redirectToRoute('app_article');
+    }
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Content = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $Category = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->Content;
+    }
+
+    public function setContent(string $Content): self
+    {
+        $this->Content = $Content;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->Category;
+    }
+
+    public function setCategory(string $Category): self
+    {
+        $this->Category = $Category;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
