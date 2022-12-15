2fc75e3065509ed05b38020dff111cc862e48f34
hamzaelkamel <elkamelh25@gmail.com>
2022-12-15 20:08:09 +0100

    /

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,10 +5,7 @@ namespace App\Entity;
 use App\Repository\ArticleRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Validator\Constraints as Assert;
 
-#[UniqueEntity('Title')]
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
 {
